82d20f9fe95c9b9238e005e598dedd648edfc5ff
Demarius Edwards <demarius.edwards@gmail.com>
2020-12-10 14:58:37 -0500

    Update ProfileContainer.js

--- src/ProfileComponents/ProfileContainer.js
+++ src/ProfileComponents/ProfileContainer.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux'
 import '../App.css'
 import CardBookmark from './CardBookmark';
-// import {  }
 
 class ProfileContainer extends Component {
   
