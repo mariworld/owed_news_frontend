1433d16a8af02fe820ab60e2c6eee497c55da41f
Demarius Edwards <demarius.edwards@gmail.com>
2020-12-10 15:00:48 -0500

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,8 +4,6 @@ import PostContainer from '../ProfileComponents/PostContainer'
 const Home = () => (
   <div>
     <h1 id="header">Owed News</h1>
-    <img src="IMG GOES HERE" alt="" />
-    
     <PostContainer />
   </div>
 );
