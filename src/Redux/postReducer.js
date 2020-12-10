11dfbc287f4675b8340dedeb3d51f32a970d2e89
Demarius Edwards <demarius.edwards@gmail.com>
2020-12-10 16:48:49 -0500

    Update postReducer.js

--- src/Redux/postReducer.js
+++ src/Redux/postReducer.js
@@ -1,26 +1,16 @@
-// let initialState = {
-//   user: {
-//     posts: []
-//   },
-//   all: []
-// }
-// import cuid from 'cuid';
-  
-let initialState = {
+ let initialState = {
   allArticles: []
 }
 
 const postReducer = (state = initialState, action) => {
 
   switch (action.type) {
-    //THIS IS MY CREATE
     case "ADD_POSTS":
 
       let copyOfArray = [...state.posts, action.payload]
-      //action.payload should be the object of new article post
+
       return {
         ...state,
-        // posts: copyOfArray
         allArticles: copyOfArray
       }
 
@@ -29,7 +19,6 @@ const postReducer = (state = initialState, action) => {
   
       return {
         ...state,
-        // posts: action.payload.articles
         allArticles: action.payload
       }
       
@@ -40,25 +29,3 @@ const postReducer = (state = initialState, action) => {
 }
 
 export default postReducer
-
-// let initialState = {
-//     posts: []
-//   }
-  
-//   let postReducer = (state = initialState, action) => {
-//     switch (action.type) {
-  
-//       case "SET_ALL_POSTS":
-  
-//       return {
-//         ...state,
-//         all: action.payload
-//       }
-  
-//       default:
-//         return state
-//     }
-//   }
-  
-  
-//   export default postReducer
